394c5ddfab5c4713c23b6904a6740c834a7a2e84
Mike Sallese <msallese31@gmail.com>
2017-02-08 20:39:06 -0500

    updates

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,22 +37,136 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(-33, 151));
 });
 
 function init() {
+    var link = document.getElementById('map');
+    // link.style.display = 'none'; //or
+    // link.style.visibility = 'hidden';
+    var baseDate = new Date("2/09/17")
+    var d = new Date();
+
+
+
+    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+
+    var diffDays = Math.round(Math.abs((baseDate.getTime() - d.getTime())/(oneDay)));
+
+
+    // var baseValue = 1;
+    console.log("diffDays: " + diffDays);
+
+    var n = d.getMonth();
+    console.log("MonthNum: " + n);
+
+
+    var diffStack = d.getMonth() - baseDate.getMonth()
+       + (12 * (d.getFullYear() - baseDate.getFullYear()));
+
+    console.log("diffstack: " + diffStack)
+    console.log("BaseDateNum: " + baseDate.getMonth());
+
+
+        if (diffDays > 0) {
+    var intro = document.getElementById('intro1');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Not in the form of: A house, a ring, or a car."
+    } 
+
+    if (diffStack > 0) {
+    var intro = document.getElementById('intro2');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "In the form of: A redeemable voucher."
+    }    
+
+    if (diffStack > 1) {
+    var intro = document.getElementById('intro3');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Made of cotton, leather, and silk."
+
+    }
+
+    if (diffStack > 2) {
+    var intro = document.getElementById('intro4');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Hand crafted by yours truly."
+    }
+
+    if (diffStack > 3) {
+    var intro = document.getElementById('intro5');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Going to need to be refined before it's fully redeemable"
+    }
+
+    if (diffStack > 4) {
+    var intro = document.getElementById('intro6');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Going to require just one more day of waiting once it is redeemed."
+    }
+
+    if (diffStack > 5) {
+    var intro = document.getElementById('intro7');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "This is going to change the text"
+    }
+
+    if (diffStack > 6) {
+    var intro = document.getElementById('intro8');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "This is going to change the text"
+    }
+
+    if (diffStack > 7) {
+    var intro = document.getElementById('intro9');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "This is going to change the text"
+    }
+
+    if (diffStack > 8) {
+    var intro = document.getElementById('intro10');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Not bound to any specific point in time."
+    }
+
+    if (diffStack > 9) {
+    var intro = document.getElementById('intro11');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Is bound to a specific location"
+    }
+
+    if (diffStack > 10) {
+    var intro = document.getElementById('intro12');
+    intro.style.display = 'block'; //or
+    intro.style.visibility = 'visible';
+    intro.innerHTML = "Probably going to require a kayak purchase"
+    }
+
+
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 1,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(-33, 151), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
+        disableDoubleClickZoom: true,
         draggable: false,
 
         // How you would like to style the map. 
@@ -164,6 +278,34 @@ function init() {
             }, {
                 "weight": 1.2
             }]
+        },
+        {
+            featureType: "administrative",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+        },
+        {
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+        },
+        {
+            featureType: "water",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+        },
+        {
+            featureType: "road",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]            
         }]
     };
 
@@ -175,11 +317,14 @@ function init() {
     map = new google.maps.Map(mapElement, mapOptions);
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
+    // var markerON = true;
+    // if (markerON == true){
+        var image = 'img/map-marker.png';
+            var myLatLng = new google.maps.LatLng(-33, 151);
+            var beachMarker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            icon: image
     });
+    // }
 }
